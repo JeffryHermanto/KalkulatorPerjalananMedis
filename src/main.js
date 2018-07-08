df29b52de100da0dd447786ed07838ae28abe822
Jeffry Hermanto <jeffryhermanto@Jeffrys-MacBook-Pro.local>
2018-07-08 20:29:46 +0700

    SASS worked

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import store from './store'
 import "./../node_modules/bulma/css/bulma.css"
 
+// Require the main Sass manifest file
+require('../mystyles.scss');
+
 Vue.config.productionTip = false
 
 new Vue({
